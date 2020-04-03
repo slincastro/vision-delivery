c3a0f7254e432549406d4e1f5b14708cf48df40e
Slin Castro <scastro@thoughtworks.com>
2020-04-03 14:23:19 -0500

    include circle Ci integration

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # vision-delivery
+
+
+install Python : 3.7.7
+
+`brew install python`
+
+install pip :
+
+`easy_install pip`
+
+Install requirements :
+
+`pip install -r requirements.txt`
+
+###Running Steps :
+
+Set the PORT env variable :
+
+`export PORT=5002`
+
+Run application :
+
+`python app.py`
---
+++ app.py
@@ -0,0 +1,20 @@
+import hashlib
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/health')
+def load_data():
+
+    return 'OK'
+
+
+if __name__ == '__main__':
+    port = os.environ["PORT"]
+    print('using port : ', port)
+    result = hashlib.md5(b'1585785600597f8475bdfc90d984a3491838c76cd9ff0729f40759496286e09ce81a517e069ebd857f')
+    print(result.hexdigest())
+    app.run(port=port, host='0.0.0.0')
